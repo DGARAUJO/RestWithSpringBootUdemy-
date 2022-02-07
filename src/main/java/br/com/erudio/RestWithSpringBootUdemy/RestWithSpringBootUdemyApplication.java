71a41516de29df1ce19665b32b82c384d0e26d4a
DGARAUJO <41165130+DGARAUJO@users.noreply.github.com>
2022-02-07 16:07:47 -0300

    Add files via upload

---
+++ src/main/java/br/com/erudio/RestWithSpringBootUdemy/RestWithSpringBootUdemyApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio.RestWithSpringBootUdemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootUdemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootUdemyApplication.class, args);
+	}
+
+}
